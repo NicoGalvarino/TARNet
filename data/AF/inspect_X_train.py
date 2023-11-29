a91c840bca272e153f47c2594d171f7249e7dda6
NicoGalvarino <70649846+NicoGalvarino@users.noreply.github.com>
2023-11-29 19:17:28 +0100

    prepare and run on generated lcs

---
+++ data/AF/inspect_X_train.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# X_train = np.load('./X_train.npy', allow_pickle=True)
+# print(X_train.shape)
+
+# y_train = np.load('./y_train.npy', allow_pickle=True)
+# print(y_train.shape)
+
+X_test = np.load('./X_test.npy', allow_pickle=True)
+print(X_test)
+
+y_test = np.load('./y_test.npy', allow_pickle=True)
+print(y_test)
--- our_script.py
+++ our_script.py
@@ -8,9 +8,8 @@ import utils, argparse, warnings, sys, shutil, torch, os, numpy as np, math
 warnings.filterwarnings("ignore")
 
 
-
 parser = argparse.ArgumentParser()
-parser.add_argument('--dataset', type=str, default='AF')
+parser.add_argument('--dataset', type=str, default='LC')
 parser.add_argument('--batch', type=int, default=128)
 parser.add_argument('--lr', type=float, default=0.01)
 parser.add_argument('--nlayers', type=int, default=2)
@@ -19,7 +18,7 @@ parser.add_argument('--nhead', type=int, default=8)
 parser.add_argument('--task_rate', type=float, default=0.5)
 parser.add_argument('--masking_ratio', type=float, default=0.15)
 parser.add_argument('--lamb', type=float, default=0.8)
-parser.add_argument('--epochs', type=int, default=300)
+parser.add_argument('--epochs', type=int, default=10)
 parser.add_argument('--ratio_highest_attention', type=float, default=0.5)
 parser.add_argument('--avg', type=str, default='macro')
 parser.add_argument('--dropout', type=float, default=0.01)
@@ -33,14 +32,14 @@ args = parser.parse_args()
 
 def main():    
     prop = utils.get_prop(args)
-    path = './data/' + prop['dataset'] + '/'
+    path = './../transformer/'
     
-    print('Data loading start...')
+    print('Data loading start...') #  dataset, data_path, task_type
     X_train, y_train, X_test, y_test = utils.data_loader(args.dataset, path, prop['task_type'])  # CHANGE INPUT
     print('Data loading complete...')
 
     print('Data preprocessing start...')
-    X_train_task, y_train_task, X_test, y_test = utils.preprocess(prop, X_train, y_train, X_test, y_test)  # CHANGE INPUT
+    X_train_task, y_train_task, X_test, y_test = utils.preprocess(prop, X_train, y_train, X_test, y_test)
     print(X_train_task.shape, y_train_task.shape, X_test.shape, y_test.shape)
     print('Data preprocessing complete...')
 
@@ -60,3 +59,27 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+'''RAN ON 10 EPOCHS
+Data loading start...
+Data loading complete...
+Data preprocessing start...
+torch.Size([512, 100, 2]) torch.Size([448]) torch.Size([128, 100, 2]) torch.Size([112])
+Data preprocessing complete...
+Initializing model...
+Model intialized...
+Training start...
+Epoch: 1, TAR Loss: 38.300318479537964 , TASK Loss: 1.3012901118823461
+Epoch: 2, TAR Loss: 9.31547874212265 , TASK Loss: 0.7666930300848824
+Epoch: 3, TAR Loss: 10.423442900180817 , TASK Loss: 0.7214092271668571
+Epoch: 4, TAR Loss: 10.836728930473328 , TASK Loss: 0.7055205873080662
+Epoch: 5, TAR Loss: 10.618939757347107 , TASK Loss: 0.6788468190601894
+Epoch: 6, TAR Loss: 10.077557921409607 , TASK Loss: 0.6665080274854388
+Epoch: 7, TAR Loss: 8.614743411540985 , TASK Loss: 0.6227702839033944
+Epoch: 8, TAR Loss: 6.918114483356476 , TASK Loss: 0.58811183486666
+Epoch: 9, TAR Loss: 6.637998044490814 , TASK Loss: 0.5583974123001099
+Epoch: 10, TAR Loss: 6.621802508831024 , TASK Loss: 0.5264310623918261
+Dataset: LC, Acc: 0.5178571428571429
+Training complete...
+'''
